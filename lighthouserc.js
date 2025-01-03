12e28649dc28326ac533b9f182d1e6397316de3a
Artur <050505art@gmail.com>
2025-01-03 21:04:35 +0200

    Added-lighthouse-to-CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,14 @@
 module.exports = {
   ci: {
     collect: {
+      url: [
+        'http://localhost:3000',
+        'http://localhost:3000/film-details',
+        'http://localhost:3000/films',
+        'http://localhost:3000/login',
+        'http://localhost:3000/register',
+        'http://localhost:3000/reviews',
+      ],
       numberOfRuns: 2, // Кількість прогонів тестів
     },
     upload: {
